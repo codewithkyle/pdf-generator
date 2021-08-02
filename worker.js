08b8c57842213a36f2dc50e1ffe5aeb2ce47b70f
Kyle Andrews <codingwithkyle@gmail.com>
2021-08-02 14:12:11 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,11 +3,14 @@ const fs = require("fs");
 const path = require("path");
 const cwd = process.cwd();
 const outDir = path.join(cwd, "output");
+const JPEGEncoder = require("./lib/jpeg-encoder");
 
 const { SOF, EOF } = workerData;
+const encoder = new JPEGEncoder(100);
 
 parentPort.addListener("message", async (item) => {
     try{
+        const imageData = Buffer.from(encoder.encode(item.barcode, 100));
 const priceData = Buffer.from(`9 0 obj
 << /Length 42 >>
 stream
@@ -30,26 +33,38 @@ endobj
 /Subtype /Image
 /Width 242
 /Height 142
-/ColorSpace /DeviceGray
-/BitsPerComponent 1
-/Filter /ASCIIHexDecode
-/Length ${Buffer.from(item.barcode).byteLength}
+/ColorSpace /DeviceRGB
+/BitsPerComponent 8
+/Filter [/ASCIIHexDecode /DCTDecode]
+/Length ${imageData.byteLength}
 >>
 stream
-${item.barcode}
+${imageData.toString("hex")}
 endstream
 endobj
 12 0 obj
 << /Length 36 >>
 stream
 q
-100 0 0 59 150 180 cm
+125 0 0 73 140 172 cm
 /Im1 Do
 Q
 endstream
 endobj
 `);
-        const bufferArray = [SOF, priceData, barcodeData, EOF];
+const labelData = Buffer.from(`13 0 obj
+<< /Length 42 >>
+stream
+BT
+/F1 24 Tf
+${(400 * 0.5) - (item.titleWidth * 0.5)} 145 Td
+0 Tc
+(${item.title}) Tj
+ET
+endstream
+endobj
+`);
+        const bufferArray = [SOF, priceData, barcodeData, labelData, EOF];
         await fs.promises.writeFile(path.join(outDir, `${item.UPC}.pdf`), Buffer.concat(bufferArray));
         parentPort.postMessage({
             type: "NEXT",
